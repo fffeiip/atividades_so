2a61bb764973660193e03efeb1021e0640d8bdf0
Felipe <felipe.escorel@gmail.com>
2021-05-11 20:55:17 -0300

    fix: :bug: Corrige algorítimo da fila
    sobe com o teste sem input do usuário

--- Atividade Prática 01/simulador_escalonamento_preemptivo.go
+++ Atividade Prática 01/simulador_escalonamento_preemptivo.go
@@ -9,7 +9,6 @@ import (
 type Process struct {
 	id                  int
 	nome                string
-	prioridade          int
 	tempo_cpu           int
 	tempo_cpu_decorrido int
 	finalizado_em       time.Time
@@ -20,28 +19,31 @@ func main() {
 	queue := list.New()
 	processes_ready := list.New()
 	var proc Process
-	var nome string
-	var tempo_cpu int
-	var quantum int
-	var total_processes int
+	// var nome string
+	// var tempo_cpu int
+	// var quantum int
+	// var total_processes int
 	var e *list.Element
 	var next *list.Element
 
 	fmt.Println("Exemplo de algorítimo Round-Robin")
 	fmt.Println("Favor informar o total de processos:")
-	fmt.Scanln(&total_processes)
+	total_processes := 8
+	// fmt.Scanln(&total_processes)
 
 	fmt.Println("Favor informar o tempo de quantum:")
-	fmt.Scanln(&quantum)
+	// fmt.Scanln(&quantum)
+	const quantum = 20
 
 	for i := 1; i <= total_processes; i++ {
 		fmt.Printf("=====\tProcesso %d\t=====\nInforme o nome do processo\n", i)
-		fmt.Scanln(&nome)
+		// fmt.Scanln(&nome)
 		fmt.Println("\nTempo de cpu:")
-		fmt.Scanln(&tempo_cpu)
+		// fmt.Scanln(&tempo_cpu)
 		proc.id = i
-		proc.nome = nome
-		proc.tempo_cpu = tempo_cpu
+		proc.nome = "pi"
+		proc.tempo_cpu = 20 * i
+		// tempo_cpu
 		queue.PushBack(proc)
 	}
 
@@ -50,27 +52,30 @@ func main() {
 	for {
 		proc = e.Value.(Process)
 		next = e.Next()
-		if proc.tempo_cpu > quantum && proc.tempo_cpu_decorrido < quantum {
-			fmt.Printf("Processo %d voltou pro final da fila\n", proc.id)
+		fmt.Printf("\n====\tProcesso %s %d\t====", proc.nome, proc.id)
+		time.Sleep(quantum * time.Millisecond)
+		if proc.tempo_cpu > quantum && proc.tempo_cpu_decorrido < proc.tempo_cpu {
+			fmt.Printf("\nProcesso voltou pro final da fila")
 			proc.tempo_cpu_decorrido = proc.tempo_cpu_decorrido + quantum
-			fmt.Println("Tempo restante para finalizar: ", proc.tempo_cpu-proc.tempo_cpu_decorrido)
+			fmt.Printf("\nTempo decorrido: %dms", proc.tempo_cpu_decorrido)
 			queue.PushBack(proc)
 		} else {
-			fmt.Printf("Processo %d finalizado\n", proc.id)
+			proc.finalizado_em = time.Now()
+			fmt.Printf("\nProcesso %d finalizado", proc.id)
+			fmt.Println("\nTempo Total: ", proc.finalizado_em.Sub(start))
 			proc.tempo_cpu_decorrido = proc.tempo_cpu_decorrido + proc.tempo_cpu
 			processes_ready.PushBack(proc)
 		}
+		fmt.Println("\n=======\tFim\t=======")
 		queue.Remove(e)
-		if next == nil && queue.Len() == 0 {
+
+		if queue.Len() == 0 {
 			break
 		}
-		e = next
+		if next == nil {
+			e = queue.Front()
+		} else {
+			e = next
+		}
 	}
-	for element := processes_ready.Front(); element != nil; element = element.Next() {
-		proc = element.Value.(Process)
-		finalizado := proc.finalizado_em.Sub(start)
-		fmt.Printf("Processo %d - %s.\n", proc.id, proc.nome)
-		fmt.Println("Tempo decorrido: ", finalizado)
-	}
-
 }
